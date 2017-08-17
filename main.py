163549cb6b936a09d87c70f83f5c3fb4da63b472
jpjoshua <jp.joshuaparker@gmail.com>
2017-08-17 15:43:47 -0400

    added more functionalty

--- main.py
+++ main.py
@@ -1,43 +1,92 @@
+import sys
 
+def main():
+    print("Please enter a number to start:")
+    print("1) Make new file")
+    print("2) Begin at the end of another file")
+    print("3) Update a file entry")
+    print("4) Exit")
 
-def output(appname, username, password, comment):
-    """This writes the 3 variables to a file."""
-    file.write("appname: %s " %(appname) + "\n")
-    file.write("username: %s " %(username) + "\n")
-    file.write("password: %s " %(password) + "\n")
-    if (comment == ""):
-        comment = "N/A"
-        file.write("Comments: %s " %(comment) + "\n")
+    choice = input("Choice: ")
+    if (choice == "1"):
+        new_file()
+        quit_program()
+
+    elif (choice == "2"):
+        resume_input()
+        quit_program()
+
+    elif (choice == "3"):
+        update_entry()
+        quit_program()
+
+    elif(choice == "4"):
+        quit_program()
+
     else:
-        file.write("Comments: %s " %(comment) + "\n")
+        print("Choose a valid option.")
+        main()
+
+def new_file():
+    global name
+    file = open("%s.txt" % (name), "w")
+    file.write("%s\'s Password Handbook" % (name) + "\n")
     file.write("***********************************\n")
+    output(file)
+    file.close()
+
+def output(file):
+    """This writes the 4 variables to a file."""
+    end = "no"
+    while (end.lower() == "no"):
+        appname = input("Please enter the Website or Application name(ex. Wellsfargo.com): ")
+        username = input("Please enter your username: ")
+        password = input("Please enter your password: ")
+        comment = input("Please enter your comment: ")
+
+        file.write("appname: %s " % (appname) + "\n")
+        file.write("username: %s " % (username) + "\n")
+        file.write("password: %s " % (password) + "\n")
+
+        if (comment == ""):
+            comment = "N/A"
+            file.write("Comments: %s " % (comment) + "\n")
+        else:
+            file.write("Comments: %s " % (comment) + "\n")
+
+        file.write("***********************************\n")
+        end = input("Are you done typing in entries (yes or no)? ")
+
+    add_note = input("Do you want to add a note section? ")
+    while True:
+        if (add_note.lower() == "no"): break
+        elif (add_note == "yes"):
+            pass
+        else:
+            print("Input yes or no only!")
+            add_note = input("Do you want to add a note section? ")
+
 
 def resume_input():
-    global file
-    with open("%s.txt" % (name), "a") as file:
-        output(appname, username, password, comment)
+    # need try catch fo file not found error
+    file = input("Please enter the exact file name: ")
+    file = open("%s.txt" % (file), "a")
+    output(file)
+    # add a system of code here that will allow you to add entries above the "Notes section"
 
 def update_entry():
+    pass
+
+def quit_program():
+    print("Thank you for using the password manager!")
+    sys.exit(0)
 
     """ Main Program """
-end = "no"
-name = input("Please Enter your first and last name: ")
-file = open("%s.txt" % (name), "w+")
+
+""" Program Starts Here """
+
+name = input("Please enter your first and last name: ")
 print("\nWelcome to %s\'s Password Handbook Creator." % (name))
-file.write("%s\'s Password Handbook" % (name) + "\n")
-file.write("***********************************\n")
-ans1 = input("Do already have a password file (yes or no)? ")
-ans2 = input("DO you want to add to an existing file (yes or no)? ")
-ans3 = input("Do you want to update one of your entries (yes or no)? ")
-print("\nIf you dont have an answer to the prompt, hit the enter button.")
+print("\nIf you don't have an answer to the prompt, hit the enter button.")
 
-
-while (end.lower() == "no" or end.lower() == "n"):
-    appname = input("Please enter the Website or Application name(ex. Wellsfargo.com): ")
-    username = input("Please enter your username: ")
-    password = input("Please enter your password: ")
-    comment = input("Please enter your comment: ")
-    output(appname, username, password, comment)
-    end = input("Are you done typing in entries (yes or no)? ")
-
-file.close()
+main()
